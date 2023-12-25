292785f1070b9c9cfd0bb8720091a3c5099ad43a
olasupo.okunaiya <olasupo.o@gmail.com>
2023-12-25 18:20:32 +0000

    First Commit

---
+++ Part1/frontend_testing.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+try:
+    # Open the web app in the browser
+    driver.get("http://127.0.0.1:5000/users/get_user_id/5")
+
+    # Wait for the page to load
+    driver.implicitly_wait(60)
+
+
+
+finally:
+    # Close the browser window
+    driver.quit()
